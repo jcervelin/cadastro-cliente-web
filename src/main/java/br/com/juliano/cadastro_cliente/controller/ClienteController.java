bc9e7bf2249f6509c01ee17b9debc4b6f5ce1231
Juliano de Sousa Cervelin <juliano.cervelin@sysmap.com.br>
2018-01-16 21:03:39 -0200

    Ajustes na criacao das queries

--- src/main/java/br/com/juliano/cadastro_cliente/controller/ClienteController.java
+++ src/main/java/br/com/juliano/cadastro_cliente/controller/ClienteController.java
@@ -10,10 +10,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import br.com.juliano.cadastro_cliente.CadastroClienteWebApplication;
 import br.com.juliano.cadastro_cliente.domain.Cliente;
 import br.com.juliano.cadastro_cliente.service.ClienteService;
 
